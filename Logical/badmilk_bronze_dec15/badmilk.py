807a480b3628de7c627ccacdcdaa326cc34b1021
osh1130 <vivizhao401@gmail.com>
2025-06-10 13:05:18 +1200

    0610

--- Logical/badmilk_bronze_dec15/badmilk.py
+++ Logical/badmilk_bronze_dec15/badmilk.py
@@ -1,18 +1,52 @@
 from collections import defaultdict
 
-fin = open('Logical/badmilk_bronze_dec15/1.in','r')
-person = defaultdict(list)  # key: person, value: list of (milk, time)
-milk_consumption = defaultdict(list)  # key: milk, value: list of (person, time)
-sick = []
+fin = open('Logical/badmilk_bronze_dec15/1.in', 'r')
+#fout = open('badmilk.out', 'w')
 
 P, M, D, S = map(int, fin.readline().split())
-for i in range(D):
-    people, milk, time = map(int, fin.readline().split())
-    milk_consumption[milk].append((people, time))
-    person[people].append((milk, time))
 
-for j in range(S):
-    p, t = map(int, fin.readline().split())
-    sick.append((p, t))
+person_drink_log = defaultdict(list)  # person -> list of (milk, time)
+milk_drink_log = defaultdict(list)    # milk -> list of (person, time)
+sick_list = []
 
-print(person)
+# 读取饮用记录
+for _ in range(D):
+    person, milk, time = map(int, fin.readline().split())
+    person_drink_log[person].append((milk, time))
+    milk_drink_log[milk].append((person, time))
+
+# 读取生病记录
+sick_time = dict()
+for _ in range(S):
+    person, time = map(int, fin.readline().split())
+    sick_list.append((person, time))
+    sick_time[person] = time
+
+max_victims = 0
+
+for milk in range(1, M + 1):
+    # 检查：所有生病的人是否在生病前喝过这瓶奶
+    is_possible = True
+    for sick_person, sick_t in sick_list:
+        drank = False
+        for m, t in person_drink_log[sick_person]:
+            if m == milk and t < sick_t:
+                drank = True
+                break
+        if not drank:
+            is_possible = False
+            break
+    if not is_possible:
+        continue
+
+    # 统计喝过这瓶奶的健康人和病人（但在生病前）
+    victims = set()
+    for person, time in milk_drink_log[milk]:
+        if person not in sick_time or time < sick_time[person]:
+            victims.add(person)
+
+    max_victims = max(max_victims, len(victims))
+
+print(max_victims)
+#fout.write(str(max_victims) + '\n')
+#fout.close()
